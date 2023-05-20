681dadc107f157b6d3d8fba73b2f1da68f4bc32d
Lê Khai Trí <lekhaitri.2003@gmail.com>
2023-05-20 16:26:07 +0700

    complete UI activity_book_details

---
+++ app/src/main/java/com/app/toko/views/BookDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.app.toko.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.toko.R;
+
+public class BookDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
